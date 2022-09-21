37f42117622f8365ad51db7c46cd79be0979d2d7
John <johnzhang@cmu.edu>
2022-09-21 16:16:55 -0400

    minor result clean up

---
+++ examples/A1-imitation/traj_opt/reach.jl
@@ -0,0 +1,3 @@
+
+include(joinpath(@__DIR__, "..", "..", "..", "examples/A1-imitation/traj_opt/models/centroidal_quadruped.jl"))
+include(joinpath(@__DIR__, "..", "..", "..", "src/dynamics/centroidal_quadruped/visuals.jl"))
